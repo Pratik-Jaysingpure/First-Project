ccd5fc0732a5b35462d1bcef4ef86a4a454afae7
Pratik Jaysingpure <pratikjaysingpure9@gmail.com>
2022-01-21 11:34:34 +0530

    first Commit

---
+++ Java/aaa.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class aaa{
+ 
+public static void main( String[]araaaa){
+
+int a;
+Scanner trop = new Scanner (System.in);
+
+System.out.println("enter the values") ;
+a =trop.nextInt();
+int  n=10;
+
+
+int  c=a+n;
+System.out.println(c);
+trop.close(); 
+
+
+}
+
+}
+
